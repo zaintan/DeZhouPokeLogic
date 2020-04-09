53ea8c623bf71a273868c041b271bc273387b198
zaintan <tzytclzxh@qq.com>
2020-04-09 15:50:18 +0800

    重构牌型类
    重构牌型类

--- card_brand.lua
+++ card_brand.lua
@@ -1,19 +1,19 @@
 --牌型类
 
-local const         = require("poke_constant")
+local const     = require("poke_constant")
 
-local AbstractBrand = class()
+local CardBrand = class()
 
-function AbstractBrand:ctor( type, cards )
+function CardBrand:ctor( type, cards )
 	self.m_type  = type
 	self.m_cards = cards
 end
 
-function AbstractBrand:getType()
+function CardBrand:getType()
 	return self.m_type
 end
 
-function AbstractBrand:compareOtherBrand( otherBrand )
+function CardBrand:compareOtherBrand( otherBrand )
 	if self:getType() == otherBrand:getType() then 
 		return self:_compareOtherBrandWithSameType(otherBrand)
 	else
@@ -21,22 +21,42 @@ function AbstractBrand:compareOtherBrand( otherBrand )
 	end  
 end
 
-function AbstractBrand:_getCompareIndexs()
-	assert("sub class must override this function!")
+local kCmpIndexsMap = {
+	[const.CardType.RoyalFlush]    = {},
+	[const.CardType.StraightFlush] = {1},
+	[const.CardType.FourOfKind]    = {1,5},
+	[const.CardType.FullHouse]     = {1,4},
+	[const.CardType.Flush]         = {1,2,3,4,5},
+	[const.CardType.Straight]      = {1},
+	[const.CardType.ThreeOfKind]   = {1,4,5},
+	[const.CardType.TwoPairs]      = {1,3,5},
+	[const.CardType.OnePairs]      = {1,3,4,5},
+	[const.CardType.HighCard]      = {1,2,3,4,5}
+}
+
+function CardBrand:_getCompareIndexs()
+	--assert("sub class must override this function!")
+	local ret = kCmpIndexsMap[self.m_type]
+	if ret then 
+		return ret
+	else 
+		print("maybe error! unknown type:", self.m_type)
+		return {}
+	end 
 end
 
-function AbstractBrand:_getCardByIndex( index )
+function CardBrand:_getCardByIndex( index )
 	assert(index >= 1 and index <= 5)
 	return self.m_cards[index]
 end
 
-function AbstractBrand:_compareCardByIndex(index, otherBrand)
+function CardBrand:_compareCardByIndex(index, otherBrand)
 	local selfValue  = self:_getCardByIndex(index):packageValue()
 	local otherValue = otherBrand:_getCardByIndex(index):packageValue()
 	return selfValue - otherValue
 end
 
-function AbstractBrand:_compareOtherBrandWithSameType( otherBrand )
+function CardBrand:_compareOtherBrandWithSameType( otherBrand )
 	local cmpIndexs = self:_getCompareIndexs()
 	local cmp = 0
 	for i=1,#cmpIndexs do
@@ -48,7 +68,7 @@ function AbstractBrand:_compareOtherBrandWithSameType( otherBrand )
 	return cmp
 end
 
-function AbstractBrand:getDesc()
+function CardBrand:getDesc()
 	local str = const.CardTypeCnDesc[self.m_type]..":"
 	for _,v in ipairs(self.m_cards or {}) do
 		str = str .. v:getDesc().." "
@@ -57,97 +77,14 @@ function AbstractBrand:getDesc()
 	return str
 end
 
-function AbstractBrand:dump()
+function CardBrand:dump()
 	print(self:getDesc())
 end
 
----------------------------------------------------------皇家同花顺------------------------------------------------------
-local RoyalFlushBrand = class(AbstractBrand)
---皇家同花顺 一定是相等的
-function RoyalFlushBrand:_getCompareIndexs()
-	return {}
-end
-
----------------------------------------------------------同花顺------------------------------------------------------
-
---同花顺
-local StraightFlushBrand = class(AbstractBrand)
-
-function StraightFlushBrand:_getCompareIndexs()
-	return {1}
-end
-
-local FourOfKindBrand = class(AbstractBrand)
-
-function FourOfKindBrand:_getCompareIndexs()
-	return {1,5}
-end
-
-local FullHouseBrand = class(AbstractBrand)
-
-function FullHouseBrand:_getCompareIndexs()
-	return {1,4}
-end
-
-local FlushBrand = class(AbstractBrand)
-
-function FlushBrand:_getCompareIndexs()
-	return {1,2,3,4,5}
-end 
-
-local StraightBrand = class(AbstractBrand)
-
-function StraightBrand:_getCompareIndexs()
-	return {1}
-end
-
-local ThreeOfKindBrand = class(AbstractBrand)
-
-function ThreeOfKindBrand:_getCompareIndexs()
-	return {1,4,5}
-end
-
-local TwoPairsBrand = class(AbstractBrand)
-
-function TwoPairsBrand:_getCompareIndexs()
-	return {1,3,5}
-end
-
-local OnePairsBrand = class(AbstractBrand)
-
-function OnePairsBrand:_getCompareIndexs()
-	return {1,3,4,5}
-end
-
-local HighCardBrand = class(AbstractBrand)
-
-function HighCardBrand:_getCompareIndexs()
-	return {1,2,3,4,5}
-end
---------------------------------------------------------------------------------------------
-local kCreateClassMap = {
-	[const.CardType.RoyalFlush]    = RoyalFlushBrand,
-	[const.CardType.StraightFlush] = StraightFlushBrand,
-	[const.CardType.FourOfKind]    = FourOfKindBrand,
-	[const.CardType.FullHouse]     = FullHouseBrand,
-	[const.CardType.Flush]         = FlushBrand,
-	[const.CardType.Straight]      = StraightBrand,
-	[const.CardType.ThreeOfKind]   = ThreeOfKindBrand,
-	[const.CardType.TwoPairs]      = TwoPairsBrand,
-	[const.CardType.OnePairs]      = OnePairsBrand,
-	[const.CardType.HighCard]      = HighCardBrand
-}
 
 local export = {}
-
 export.createBrand = function ( type, cards )
-	local clsname = kCreateClassMap[type]
-	if clsname then 
-		return new(clsname, type, cards)
-	else
-		print("may be error! unknown type:", type)
-	end 
-	return nil
+	return new(CardBrand, type, cards)
 end
 
 return export
